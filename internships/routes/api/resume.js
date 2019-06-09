d0e701b878907bde8ba00a92a2963aa2a404f6db
sanjay-07_singh <sanjaysinghbisht114@gmail.com>
2019-06-09 15:37:04 +0530

    Done three more routes in resume

--- internships/routes/api/resume.js
+++ internships/routes/api/resume.js
@@ -137,14 +137,75 @@ router.post('/adde',passport.authenticate('jwt',{session:false}),(req,res)=>{
 @access - PRIVATE
 */
 router.delete('/dele-:eid',passport.authenticate('jwt',{session:false}),(req,res)=>{
-    
+Profile.findOne({user:req.user._id})
+       .then(profile=>{
+ Resume.findOne({user:profile._id})
+       .then(resume=>{
+       const i=resume.education.findIndex(a=>a._id.toString()==req.params.eid.toString());
+       resume.education.splice(i,1);
+       resume.save()
+             .then(resume=>res.status(200).json(resume))
+             .catch(err=>console.log('Connection error'));
+            })
+                 .catch(err=>console.log('Connection error'));
+       })
+       .catch(err=>console.log('Connection error'));
 });
 
 
+/*
+@type - POST
+@route - /api/resume/addj
+@desc - a route to add job details in the resume of an intern
+@access - PRIVATE
+*/
+router.post('/addj',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    Profile.findOne({user:req.user._id})
+           .then(profile=>{
+               Resume.findOne({user:profile._id})
+                     .then(resume=>{
+                         const jobs={};
+                         if(req.body.title)
+                         jobs.title=req.body.title;
+                         if(req.body.year)
+                         jobs.year=req.body.year;
+                         if(req.body.institution)
+                         jobs.institution=req.body.institution;
+                         if(req.body.text)
+                         jobs.text=req.body.text;
+                         resume.jobs.unshift(jobs);
+                         resume.jobs=resume.jobs.sort((a,b)=>a.year-b.year);
+                         resume.save()
+                              .then(resume=>res.status(200).json(resume))
+                              .catch(err=>console.log('Connection error'));
+                     })
+                     .catch(err=>console.log('Connection error'));
+           })
+           .catch(err=>console.log('Connection error'));
+});
 
 
-
-
+/*
+@type - DELETE
+@route - /api/resume/delj-:jid
+@desc - a route to remove job details in the resume of an intern
+@access - PRIVATE
+*/
+router.delete('/delj-:jid',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    Profile.findOne({user:req.user._id})
+           .then(profile=>{
+     Resume.findOne({user:profile._id})
+           .then(resume=>{
+           const i=resume.jobs.findIndex(a=>a._id.toString()==req.params.jid.toString());
+           resume.jobs.splice(i,1);
+           resume.save()
+                 .then(resume=>res.status(200).json(resume))
+                 .catch(err=>console.log('Connection error'));
+                })
+                     .catch(err=>console.log('Connection error'));
+           })
+           .catch(err=>console.log('Connection error'));
+    });
 
 
 
