ccf94f5883000b619f3e6b9244fe355497fc5f63
SANJAY SINGH BISHT <39243846+SANJAY072000@users.noreply.github.com>
2019-06-21 12:36:54 +0530

    done2

--- internships/setup/myurl.js
+++ internships/setup/myurl.js
@@ -1,5 +1,5 @@
 // exporting things that don't change
 module.exports={
-mongourl:"mongodb+srv://sanjay_20:san07jay@cluster0-yxg19.mongodb.net/test?retryWrites=true&w=majority",
+mongourl:"",
 secret:"strong secret of mine"
 };
